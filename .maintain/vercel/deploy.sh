d35458f9ff2dc7e415449c6ff78f3f28027bb376
fewensa <fewensa@protonmail.com>
2021-09-01 17:29:31 +0800

    remove main branch

--- .maintain/vercel/deploy.sh
+++ .maintain/vercel/deploy.sh
@@ -13,11 +13,6 @@ if [[ "$VERCEL_GIT_COMMIT_REF" == "master" ]] ; then
   exit 0
 fi
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
-  yarn build
-  exit 0
-fi
-
 if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" ]] ; then
   yarn build
   exit 0
