7f058f176ac1781622ec05ac892b5c18521f02dc
fewensa <fewensa@protonmail.com>
2021-09-01 17:26:40 +0800

    test ci

--- .maintain/vercel/deploy.sh
+++ .maintain/vercel/deploy.sh
@@ -8,9 +8,20 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 yarn install
 
-if [[ "$VERCEL_ENV" == "production" ]] ; then
-    yarn build
-  else
-    yarn build:dev
+if [[ "$VERCEL_GIT_COMMIT_REF" == "master" ]] ; then
+  yarn build
+  exit 0
 fi
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
+  yarn build
+  exit 0
+fi
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" ]] ; then
+  yarn build
+  exit 0
+fi
+
+yarn build:dev
 
