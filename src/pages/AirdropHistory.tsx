e989a3e26725d7c1fb46e74612716a55e08dfeb4
sxlwar <sxlwar@163.com>
2021-09-04 19:32:37 +0800

    chore: move history entrance to header

--- src/pages/AirdropHistory.tsx
+++ src/pages/AirdropHistory.tsx
@@ -6,7 +6,7 @@ export function AirdropHistory() {
   return (
     <Card
       className="xl:w-1/3 lg:1/2 md:w-2/3 w-full mx-auto drop-shadow  dark:border-transparent"
-      title={<Trans>Airdrop History</Trans>}
+      title={<Trans>Airdrop Records</Trans>}
     >
       <AirdropRecords />
     </Card>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { Card } from 'antd';
-import { Trans } from 'react-i18next';
-import { Link, RouteComponentProps } from 'react-router-dom';
-import { HistoryLink } from '../components/HistoryLink';
+import { RouteComponentProps } from 'react-router-dom';
 import { TransferForm } from '../components/TransferForm';
 import { Path } from '../config/routes';
 import { DepartureProvider } from '../providers';
@@ -17,18 +15,6 @@ export function Home(props: RouteComponentProps) {
       <Card className="xl:w-1/3 lg:1/2 md:w-2/3 w-full mx-auto drop-shadow dark:border-transparent">
         <TransferForm isCross={isCross}></TransferForm>
       </Card>
-
-      <p className="mt-4 text-center xl:w-1/3 lg:1/2 md:w-2/3 w-full mx-auto">
-        {isCross ? (
-          <Trans i18nKey="viewCrossHistory" className="mt-4 text-center">
-            You can view your <HistoryLink>Cross-chain history</HistoryLink>
-          </Trans>
-        ) : (
-          <Trans i18nKey="viewAirdropHistory" className="mt-4 text-center">
-            You can view your <Link to={Path.airdropHistory}>Airdrop history</Link>
-          </Trans>
-        )}
-      </p>
     </DepartureProvider>
   );
 }
