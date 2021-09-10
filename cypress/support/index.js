220772da98983a04e317f771de58e52c50273f85
sxlwar <sxlwar@163.com>
2021-09-10 15:01:56 +0800

    chore: test e2e

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import '@carlos0202/cypress-metamask';
+// import '@carlos0202/cypress-metamask';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
