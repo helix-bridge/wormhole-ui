e4bfdb7f4af450b68cf36ee4f10d0e703a421b17
sxlwar <sxlwar@163.com>
2021-09-21 18:46:37 +0800

    chore: temp commit

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -15,7 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-// import '@carlos0202/cypress-metamask';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
