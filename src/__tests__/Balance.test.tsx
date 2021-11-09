d5cd93d0bd134fa478dacab2d484200a2bc6a9cf
sxlwar <sxlwar964@gmail.com>
2021-11-09 15:57:00 +0800

    test: add test case for balance component (#186)

--- src/__tests__/Balance.test.tsx
+++ src/__tests__/Balance.test.tsx
@@ -5,40 +5,54 @@ import '../index.scss';
 import '../theme/antd/index.less';
 
 describe('Balance Component', () => {
-  it('use default precision (9)', () => {
-    mount(
-      <Suspense fallback="loading">
-        <Balance />
-      </Suspense>
-    );
+  it('should use default precision 9 to display', () => {
+    mount(<Balance />);
 
     cy.waitForReactComponent();
-    cy.react('InputNumber').find('input').type('123.111029298382');
-    cy.react('InputNumber').find('input').blur();
-    cy.react('InputNumber').find('input').should('have.value', '123.111029298');
+    cy.react('InputNumber').find('input').as('balance');
+    cy.get('@balance').type('123.111029298382');
+    cy.get('@balance').blur();
+    cy.get('@balance').should('have.value', '123.111029298');
 
-    cy.react('InputNumber').find('input').clear();
-    cy.react('InputNumber').find('input').type('2');
-    cy.react('InputNumber').find('input').blur();
-    cy.react('InputNumber').find('input').should('have.value', '2.000000000');
+    cy.get('@balance').clear();
+    cy.get('@balance').type('2');
+    cy.get('@balance').blur();
+    cy.get('@balance').should('have.value', '2.000000000');
 
-    cy.react('InputNumber').find('input').clear();
-    cy.react('InputNumber').find('input').type('-1');
-    cy.react('InputNumber').find('input').blur();
-    cy.react('InputNumber').find('input').should('have.value', '0.000000000');
+    cy.get('@balance').clear();
+    cy.get('@balance').type('-1');
+    cy.get('@balance').blur();
+    cy.get('@balance').should('have.value', '0.000000000');
   });
 
-  it.skip('output value', () => {
-    const fn = cy.spy();
-    mount(
-      <Suspense fallback="loading">
-        <Balance onChange={fn} />
-      </Suspense>
-    );
+  it('should output the typed value', () => {
+    const fn = cy.stub().as('listener');
+    mount(<Balance onChange={fn} />);
 
     cy.waitForReactComponent();
-    cy.react('InputNumber').find('input').type('12345');
-    cy.react('InputNumber').find('input').blur();
-    expect(fn).to.be.called.calledWith('12345.000000000');
+    cy.react('InputNumber').find('input').as('balance');
+    cy.get('@balance').type('123.45');
+    cy.get('@listener')
+      .should('have.been.called')
+      .its('lastCall.args')
+      .then(([arg]) => {
+        expect(arg).eq('123.45');
+      });
+  });
+
+  it('should output unformatted value', () => {
+    const fn = cy.stub().as('listener');
+    mount(<Balance value="123,456.7" onChange={fn} />);
+
+    cy.waitForReactComponent();
+    cy.react('InputNumber').find('input').as('balance');
+    cy.get('@balance').focus();
+    cy.get('@balance').blur();
+    cy.get('@listener')
+      .should('have.been.called')
+      .its('lastCall.args')
+      .then(([arg]) => {
+        expect(arg).eq('123456.7');
+      });
   });
 });
