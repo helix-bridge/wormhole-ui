eac090618a2ca0c47a10ff3a3c562ad9e63c33bf
sxlwar <sxlwar@163.com>
2022-04-08 20:30:34 +0800

    chore: add pro apollo endpoint

--- src/config/client.ts
+++ src/config/client.ts
@@ -3,5 +3,5 @@ import { GraphQLClient } from 'graphql-hooks';
 const isDev = process.env.REACT_APP_HOST_TYPE === 'dev';
 
 export const client = new GraphQLClient({
-  url: isDev ? 'http://localhost:4000/' : '',
+  url: isDev ? 'http://localhost:4000/' : 'https://wormhole-apollo.darwinia.network/',
 });
